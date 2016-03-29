7327aaee70b075bcd4f57b5981131f54e7265e72
fis <fis@fiss-mac-2.local>
2016-03-29 12:47:56 +0530

    Initial Commit

---
+++ SourceControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceControl
+//
+//  Created by fis on 29/03/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SourceControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SourceControl
+//
+//  Created by fis on 29/03/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
